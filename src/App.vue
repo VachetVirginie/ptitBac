1aea8c6d91771b84ba87093097c412d06988bd17
Vivi <vachetvirginie@gmail.com>
2020-11-12 17:18:23 +0100

    feat(style): first steps on style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <img class="rotate" src="./assets/logo.svg">
-  <button v-on:click="generateLetter()">Lettre</button>
-  <p>{{ message }} </p>
-  <p>{{ letters }}</p>
-  <div v-if="isGenerated">
-    <p> {{ error }}</p>
+
+  <div class="max-w-sm rounded overflow-hidden shadow-lg flex content-center flex flex-col">
+    <div class="ml-24 mb-4">
+      <img class="animate-spin w-20 grid grid-cols-1" src="./assets/logo.svg">
+    </div>
+    <p>Nouvelle lettre: <br>{{ message }} </p>
+    <p>Lettres déjà jouées: <br>{{ letters }}</p>
+    <div class="px-6 py-4">
+      <div v-if="isGenerated">
+        <p> {{ error }}</p>
+      </div>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <button class="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded" v-on:click="generateLetter()">Lettre</button>
+    </div>
   </div>
 </template>
 
@@ -16,7 +25,7 @@ export default {
     return {
       message: '',
       letters: [],
-      error: 'Je cherche une nouvelle lettre qui ne sois pas encore sortie',
+      error: 'Je cherche une nouvelle lettre qui ne soit pas encore sortie',
       isGenerated: false
     }
   },
