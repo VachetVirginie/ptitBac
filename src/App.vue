220c26f8350016b16f4ff512c3dbf9cebda7b22d
Vivi <vachetvirginie@gmail.com>
2020-11-12 16:07:49 +0100

    fix(init)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="rotate" src="./assets/logo.svg">
+  <button v-on:click="generateLetter()">Lettre</button>
+  <p>{{ message }} </p>
+  <p>{{ letters }}</p>
+  <div v-if="isGenerated">
+    <p> {{ error }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      message: '',
+      letters: [],
+      error: 'Je cherche une nouvelle lettre qui ne sois pas encore sortie',
+      isGenerated: false
+    }
+  },
   components: {
-    HelloWorld
-  }
+  },
+  methods: {
+      rndStr(len) {
+        let text = ""
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        for( let i=0; i < len; i++ ) {
+          text += chars.charAt(Math.floor(Math.random() * chars.length))
+        }
+        return text
+      },
+    generateLetter() {
+      let message = this.rndStr(1)
+      if(this.letters.indexOf(message) == -1) {
+        this.message = message
+        this.letters += message + '  '
+        this.isGenerated = false
+
+      }
+      else {
+        this.isGenerated = true
+        return this.error
+      }
+    }
+    },
+    mounted () {
+
+    }
 }
 </script>
 
@@ -22,5 +60,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.rotate {
+  transition: transform 1s ease-in-out;
+  height: 20vmin;
+}
+.rotate:hover {
+  transform: rotateZ(360deg);
 }
 </style>
